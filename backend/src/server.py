2c1d416eaf03215f4a7bc6c1baaa49a764221dac
williamhCode <williamh.coding@gmail.com>
2025-10-02 13:07:12 -0400

    update

--- backend/src/server.py
+++ backend/src/server.py
@@ -46,8 +46,12 @@ app.add_middleware(
 )
 
 # database setup -----------------------------------------------
-db_url = "sqlite:///./receipt-helper.db"
-engine = create_engine(db_url, connect_args={"check_same_thread": False})
+db_url = os.getenv("DATABASE_URL")
+if db_url is None:
+    db_url = "sqlite:///./receipt-helper.db"
+    engine = create_engine(db_url, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(db_url, pool_pre_ping=True)
 Base.metadata.create_all(bind=engine)
 
 
